d399206cc5e963cff11f6f597507ecbf1aa6af5a
Lucas Stefanski <stefaluc000@gmail.com>
2016-08-22 22:45:06 -0500

    Completed first version of site

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+	setTimeout(function() {
+		document.querySelectorAll('.link')[0].style.opacity = '1';
+		document.querySelectorAll('.link')[1].style.opacity = '1';
+		document.querySelectorAll('.link')[2].style.opacity = '1';
+		document.getElementById('photo').style.opacity = '1';
+		document.getElementById('divider').style.width = '50%';
+	}, 1000);
+}
