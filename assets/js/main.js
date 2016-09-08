27e7b239c7f64d4e56e5eb68b8df5c5bb9d78593
Lucas Stefanski <stefaluc000@gmail.com>
2016-09-08 00:40:31 -0500

    Updated background. Added email icon.

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@ window.onload = function() {
 		document.querySelectorAll('.link')[0].style.opacity = '1';
 		document.querySelectorAll('.link')[1].style.opacity = '1';
 		document.querySelectorAll('.link')[2].style.opacity = '1';
+		document.querySelectorAll('.link')[3].style.opacity = '1';
 		document.getElementById('photo').style.opacity = '1';
 		document.getElementById('divider').style.width = '50%';
 	}, 1000);
