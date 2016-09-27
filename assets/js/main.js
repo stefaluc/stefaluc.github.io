a1a798f79eb050f05c072cef31c67e06adb51a6d
Lucas Stefanski <stefaluc000@gmail.com>
2016-09-26 23:47:08 -0500

    This is a shit show

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,3 @@
-$(document).ready(function(){
-	$('.slider').slider({full_width: true});
-});
-
 window.onload = function() {
 	setTimeout(function() {
 		document.querySelectorAll('.link')[0].style.opacity = '1';
@@ -13,10 +9,80 @@ window.onload = function() {
 	}, 1000);
 }
 
-$('.arrow-next').click(function() {
-	$('.slider').slider('next');
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Main = React.createClass({
+	getInitialState: function() {
+		console.log('reached');
+		return {
+			progression: 0
+		};
+	},
+
+	next: function() {
+		this.setState({
+			progression: this.state.progression++
+		});
+	},
+
+	prev: function() {
+		this.setState({
+			progression: this.state.progression++
+		});
+	},
+
+	render: function() {
+		var components;
+		var state = this.state.progression;
+
+		if(state === 0) {
+			components = (
+				<div>
+				<div className='arrow-container'>
+				</div>
+
+				<div id='container'>
+					<div id='photo'>
+						<img src="../images/me.jpg" />
+					</div>
+					<h1>Lucas Stefanski</h1>
+					<div id="divider"></div>
+					<div id="description">
+						Studying Computer Science with a Certificate in Mathematics at the University of Wisconsin - Madison. Lover of all things technology, science, and math. Get to know me better at the links below.
+					</div>
+					<div id="links">
+						<div className="link">
+					        <a href="../resume.pdf" target="blank"><i className="ion-android-document custom-icon resume"></i></a>
+					      	<span className="tooltiptext">Resume</span>
+						</div>
+						<div className="link">
+							<a href="https://github.com/stefaluc" target="blank"><i className="ion-social-github custom-icon github"></i></a>
+							<span className="tooltiptext">Github</span>
+						</div>
+						<div className="link">
+					        <a href="mailto:stefaluc000@gmail.com" target="top"><i className="ion-email custom-icon email"></i></a>
+					      	<span className="tooltiptext">Email</span>
+						</div>
+						<div className="link">
+							<a href="https://www.linkedin.com/in/lucas-stefanski-364889126" target="blank"><i className="ion-social-linkedin custom-icon linkedin"></i></a>
+							<span className="tooltiptext">LinkedIn</span>
+						</div>
+					</div>
+				</div>
+
+				<div className='arrow-container'>
+					<div className='arrow-next'></div>
+				</div>
+				</div>
+			);
+		}
+		console.log(components);
+		return components;
+	}
 });
 
-$('.arrow-prev').click(function() {
-	$('.slider').slider('prev');
+ReactDOM.render(<Main />, function() {
+	console.log('reached');
+	document.body;
 });
